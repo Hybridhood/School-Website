0456550cfcbbf259f6c0fde706809969f29b0c57
Benjamin Hogus <67559008+Hybridhood@users.noreply.github.com>
2026-01-24 01:27:35 +0000

    modified:   Website not for school/static/css/gray.css 	modified:   Website not for school/static/js/test.js 	modified:   Website not for school/templates/Gray.html

--- Website not for school/static/js/test.js
+++ Website not for school/static/js/test.js
@@ -1,54 +1,8 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const cont = document.getElementById("cont");
     const cch = cont.children
-    
 
-    cch[0].addEventListener('mouseenter', function(event) {
-        let x = event.clientX, y = event.clientY;
-        selected = document.elementFromPoint(x, y);
-        if (selected == cont) {return;}
-        selected.style.width = "50%";
-    });
-    cch[0].addEventListener('mouseleave', function() {
-        if (selected == cont) {
-            return;
-        }
-        selected.style.width = "25%";
-    });
-    cch[1].addEventListener('mouseenter', function(event) {
-        let x = event.clientX, y = event.clientY;
-        selected = document.elementFromPoint(x, y);
-        if (selected == cont) {return;}
-        selected.style.width = "50%";
-    });
-    cch[1].addEventListener('mouseleave', function() {
-        if (selected == cont) {
-            return;
-        }
-        selected.style.width = "25%";
-    });
-    cch[2].addEventListener('mouseenter', function(event) {
-        let x = event.clientX, y = event.clientY;
-        selected = document.elementFromPoint(x, y);
-        if (selected == cont) {return;}
-        selected.style.width = "50%";
-    });
-    cch[2].addEventListener('mouseleave', function() {
-        if (selected == cont) {
-            return;
-        }
-        selected.style.width = "25%";
-    });
-    cch[3].addEventListener('mouseenter', function(event) {
-        let x = event.clientX, y = event.clientY;
-        selected = document.elementFromPoint(x, y);
-        if (selected == cont) {return;}
-        selected.style.width = "50%";
-    });
-    cch[3].addEventListener('mouseleave', function() {
-        if (selected == cont) {
-            return;
-        }
-        selected.style.width = "25%";
-    });
-});
+}); 
+function expand(element) {
+    element.innerHTML="Hello";
+}
